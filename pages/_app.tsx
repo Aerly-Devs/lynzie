aec920a58754540c77316688835e4ae36d601121
danielbatres <danielbatresuser@gmail.com>
2022-08-11 14:44:30 -0600

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { AppProps } from 'next/app';
-import "@/styles/global.css";
+import "@/styles/global/global.css";
 
 function App({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
