f05e32ddc0ea12c53f4d09aed78b128316d6b9ea
danielbatres <danielbatresuser@gmail.com>
2022-08-10 19:51:16 -0600

    Setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,27 @@
-import Link from 'next/link';
-import React from 'react';
-import 'Navbar.module.sass';
+import Link from "next/link";
+import React from "react";
+import "@styles/Navbar.module.sass";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link href="">
-            <a>Lynzie</a>
-        </Link>
-        <ul>
-            <li>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-            </li>
-        </ul>
+      <Link href="/">
+        <a>Lynzie</a>
+      </Link>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
