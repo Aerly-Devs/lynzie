7c45d37fba0b258382cb39ab50c636764619c0b4
danielbatres <danielbatresuser@gmail.com>
2022-08-11 13:43:30 -0600

    about file

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>About | Lynzie</title>
+      </Head>
+      <div>About</div>
+    </React.Fragment>
+  );
+}
+
+export default About;
