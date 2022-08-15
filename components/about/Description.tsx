a35207ca1d361458ed430efe8c53716bc0758d63
danielbatres <danielbatresuser@gmail.com>
2022-08-15 10:45:17 -0600

    About description component

---
+++ components/about/Description.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from '@/styles/about/Description.module.sass';
+
+const Description = () => {
+  return (
+    <>
+      <div className={styles.paragraph}>
+        <h2 className={styles.title}>About</h2>
+        <p className={styles["description-text"]}>
+          Desarrolladores que estuvieron a cargo de la creacion de la aplicacion
+          “LYNZIE”
+        </p>
+      </div>
+      <div className={styles.paragraph}>
+        <h2 className={styles.title}>Desarrollo y Diseño de la aplicación</h2>
+        <p className={styles["description-text"]}>
+          Programación del sitio web, algoritmos, Secciones, diseño de interfas,
+          creador de las funciones principales.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Description;
