b613239ae08723027d8baf7e22cbf4181cb4b748
danielbatres <danielbatresuser@gmail.com>
2022-08-17 21:13:37 -0600

    Home targets updated

--- components/home/Targets.tsx
+++ components/home/Targets.tsx
@@ -1,39 +1,36 @@
 import React from 'react';
-import styles from '@/styles/home/Targets.module.sass';
 import Link from 'next/link';
 
-const Targets = () => {
+const Targets = ({ styles }) => {
   return (
-    <section className={styles["Targets-container"]}>
-      <div className={styles.title}>
-        <h2>Ecuaciones Químicas</h2>
+    <section className={styles.ecuaciones}>
+      <h2>Ecuaciónes Quimicas</h2>
+      <div className={styles["targets_container"]}>
+        <figure className={styles.target}>
+          <div className={styles["text_target"]}>
+            <h3>Balanceo</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+              nostrum?
+            </p>
+            <Link href="#">
+              <a>COMENZAR</a>
+            </Link>
+          </div>
+        </figure>
+        <figure className={styles.target}>
+          <div className={styles["text_target"]}>
+            <h3>Balanceo</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
+              nostrum?
+            </p>
+            <Link href="#">
+              <a>COMENZAR</a>
+            </Link>
+          </div>
+        </figure>
       </div>
-      <section className={styles.targets}>
-        <article className={styles["first-target"]}>
-          <h3>Balanceo</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
-            nostrum?
-          </p>
-          <button>
-            <Link href="/">
-              <a>COMENZAR</a>
-            </Link>
-          </button>
-        </article>
-        <article className={styles["second-target"]}>
-          <h3>Balanceo</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum,
-            nostrum?
-          </p>
-          <button>
-            <Link href="/">
-              <a>COMENZAR</a>
-            </Link>
-          </button>
-        </article>
-      </section>
     </section>
   );
 }
--- templates/home/MainHome.tsx
+++ templates/home/MainHome.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from '@/components/home/Home';
 import Targets from '@/components/home/Targets';
+import styles from "@/styles/home/Targets.module.scss";
 
 const MainHome = () => {
   return (
     <main>
       <Home />
-      <Targets />
+      <div className={styles.container}>
+        <Targets styles={styles} />
+      </div>
     </main>
   );
 }
