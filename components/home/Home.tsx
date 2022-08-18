c317953e04a168e221f52862fe06cd0efa812509
danielbatres <danielbatresuser@gmail.com>
2022-08-17 19:32:29 -0600

    home updated

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -5,26 +5,25 @@ import styles from '@/styles/home/Home.module.sass';
 
 const Home = () => {
   return (
-    <section className={styles["cont_header container"]}>
-      <div className={styles["text_header"]}>
-        <h1>LYNZIE</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          aperiam!
-        </p>
-        <Link href="#">
-          <a>COMENZAR</a>
-        </Link>
+    <>
+      <div className={styles.container}>
+        <section className={styles["cont_header"]}>
+          <div className={styles["text_header"]}>
+            <h1>LYNZIE</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              aperiam!
+            </p>
+            <Link href="#">
+              <a>COMENZAR</a>
+            </Link>
+          </div>
+          <div className={styles["img_header"]}>
+            <img src="https://i.ibb.co/th7mxZ8/atom.png" alt="" />
+          </div>
+        </section>
       </div>
-      <div className={styles["img_header"]}>
-        <Image
-          src="https://i.ibb.co/th7mxZ8/atom.png"
-          alt=""
-          width={400}
-          height={400}
-        />
-      </div>
-    </section>
+    </>
   );
 }
 
