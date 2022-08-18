48605edb5ebf7977a1aec34702e7b9b6ae604987
danielbatres <danielbatresuser@gmail.com>
2022-08-17 19:44:00 -0600

    About targets

---
+++ components/about/Target.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/about/Targets.module.sass";
+
+const Target = ({ name, instagram, github, linkedin, twitter }) => {
+  return (
+    <div className={styles.cards}>
+      <div className={styles.card}>
+        <h2 className={styles["title-card"]}>Desarrollador</h2>
+        <h3 className={styles["card-description"]}>{name}</h3>
+        <hr />
+        <h3 className={styles["title-card"]}>Contáctalo</h3>
+        <p className={styles["card-description"]}>Sus redes sociales</p>
+        <div className={styles["icons-container"]}>
+          <figure>
+            <div className={styles["icon-container"]}>
+              <Link href={instagram} target="_blank">
+                <a>
+                  <Image
+                    src="./icons/instagram.svg"
+                    className="icon"
+                    alt="Instagram icon"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </Link>
+            </div>
+          </figure>
+          <figure>
+            <div className={styles["icon-container"]}>
+              <Link href={github} target="_blank">
+                <a>
+                  <Image
+                    src="./icons/github.svg"
+                    className="icon"
+                    alt="gitHub icon"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </Link>
+            </div>
+          </figure>
+          <figure>
+            <div className={styles["icon-container"]}>
+              <Link href={linkedin} target="_blank">
+                <a>
+                  <Image
+                    src="./icons/linkedin.svg"
+                    className="icon"
+                    alt="LinkedIn icon"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </Link>
+            </div>
+          </figure>
+          <figure>
+            <div className={styles["icon-container"]}>
+              <Link href={twitter} target="_blank">
+                <a>
+                  <Image
+                    src="./icons/twitter.svg"
+                    className="icon"
+                    alt="Twitter icon"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </Link>
+            </div>
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Target;
--- components/about/Targets.tsx
+++ components/about/Targets.tsx
@@ -1,177 +1,42 @@
 import React from "react";
-import styles from "@/styles/about/Targets.module.sass";
+import Target from "@/components/about/Target";
 
 const Targets = () => {
+  const developers = [
+    {
+      name: "Daniel Alessandro García Batres",
+      instagram: "#",
+      github: "#",
+      linkedin: "#",
+      twitter: "#",
+    },
+    {
+      name: "Héctor Ramón Rivera González",
+      instagram: "#",
+      github: "#",
+      linkedin: "#",
+      twitter: "#",
+    },
+    {
+      name: "Erick Wilfredo Cáceres González",
+      instagram: "#",
+      github: "#",
+      linkedin: "#",
+      twitter: "#",
+    },
+  ];
+
   return (
     <>
-      <div className={styles.cards}>
-        <div className={styles.card}>
-          <h2 className={styles["title-card"]}>Desarrollador</h2>
-          <h3 className={styles["card-description"]}>
-            Daniel Alessandro García Batres
-          </h3>
-          <hr />
-          <h3 className={styles["title-card"]}>Contáctalo</h3>
-          <p className={styles["card-description"]}>Sus redes sociales</p>
-          <div className={styles["icons-container"]}>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/instagram.svg"
-                    className="icon"
-                    alt="Instagram icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/github.svg"
-                    className="icon"
-                    alt="gitHub icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/linkedin.svg"
-                    className="icon"
-                    alt="LinkedIn icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/twitter.svg"
-                    className="icon"
-                    alt="Twitter icon"
-                  />
-                </a>
-              </div>
-            </figure>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h2 className="title-card">Desarrollador</h2>
-          <h3 className={styles["card-description"]}>
-            Héctor Ramón Rivera González
-          </h3>
-          <hr />
-          <h3 className="title-card">Contáctalo</h3>
-          <p className={styles["card-description"]}>Sus redes sociales</p>
-
-          <div className={styles["icons-container"]}>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/instagram.svg"
-                    className="icon"
-                    alt="Instagram icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/github.svg"
-                    className="icon"
-                    alt="gitHub icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/linkedin.svg"
-                    className="icon"
-                    alt="LinkedIn icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/twitter.svg"
-                    className="icon"
-                    alt="Twitter icon"
-                  />
-                </a>
-              </div>
-            </figure>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h2 className={styles["title-card"]}>Desarrollador</h2>
-          <h3 className={styles["card-description"]}>
-            Erick Wilfredo Cáceres González
-          </h3>
-          <hr />
-          <h3 className={styles["title-card"]}>Contáctalo</h3>
-          <p className={styles["card-description"]}>Sus redes sociales</p>
-          <div className={styles["icons-container"]}>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/instagram.svg"
-                    className="icon"
-                    alt="Instagram icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/github.svg"
-                    className="icon"
-                    alt="gitHub icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/linkedin.svg"
-                    className="icon"
-                    alt="LinkedIn icon"
-                  />
-                </a>
-              </div>
-            </figure>
-            <figure>
-              <div className={styles["icon-container"]}>
-                <a href="#" target="_blank">
-                  <img
-                    src="./icons/twitter.svg"
-                    className="icon"
-                    alt="Twitter icon"
-                  />
-                </a>
-              </div>
-            </figure>
-          </div>
-        </div>
-      </div>
+      {developers.map((developer) => {
+        <Target
+          name={developer.name}
+          instagram={developer.instagram}
+          github={developer.github}
+          linkedin={developer.linkedin}
+          twitter={developer.twitter}
+        />;
+      })}
     </>
   );
 };
